cbc865bc576979c7cb07c55ce64e8041e9e4f5e5
SylvainSbrr <sylvainsouber@hotmail.fr>
2022-05-05 14:11:46 +0200

    Initial commit

---
+++ src/main/java/com/notemediscreen/notemediscreen/NotemediscreenApplication.java
@@ -0,0 +1,13 @@
+package com.notemediscreen.notemediscreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotemediscreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotemediscreenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notemediscreen/notemediscreen/NotemediscreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notemediscreen.notemediscreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotemediscreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
